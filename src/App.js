53525dc833bc19df904396576ad642911eb69f65
munajaf <munajaf21@gmail.com>
2021-09-30 20:23:59 +0800

    change import to react lazy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import "./styles.css";
-import User from "./User";
+import { lazy, Suspense } from "react";
+const User = lazy(() => import("./User"));
 
 export default function App() {
   return (
     <div>
-      <User userId={1} />
-      <User userId={2} />
-      <User userId={3} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <User userId={1} />
+        <User userId={2} />
+        <User userId={3} />
+      </Suspense>
     </div>
   );
 }
